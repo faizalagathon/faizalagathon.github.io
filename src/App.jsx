ad5e6694932ea05931157a718cccc04a87d195db
FaizalAgathon <faizalagathon06@gmail.com>
2023-07-16 19:30:33 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Pricing from "./pages/Pricing"
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
